7870bd36fc04b9375724c112f26f0b74fe2a5697
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-20 11:22:20 +0530

    [Arun] Added array test practice program

---
+++ ArraysTest/placeInArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysTest
+{
+    class placeInArray
+    {
+        public static void placer()
+        {
+            Console.WriteLine("Enter number of numbers you want to add : ");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            int[] arrayStorage = new int[userValue];
+            Console.WriteLine("An array storage has been created");
+            for (int index = 0; index < userValue; index++)
+            {
+                arrayStorage[index] = Convert.ToInt32(Console.ReadLine());
+            }
+            //Console.WriteLine("These are all the elements that are being summed up : ");
+            //for (int index=0; index < arrayStorage.Length; index++)
+            //{
+            //    Console.WriteLine(arrayStorage[index] + " ");
+            //}
+            int sum = 0;
+            for (int index = 0; index < arrayStorage.Length; index++)
+            {
+                sum = sum + arrayStorage[index];
+            }
+            Console.WriteLine("This is the sum of all the elements in an array : " + sum);
+        }
+    }
+}
