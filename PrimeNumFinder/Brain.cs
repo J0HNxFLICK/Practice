5c293817590afda78af7c358660c711f47eb4f9b
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-20 11:31:17 +0530

    [Arun] Added prime number program that can also check with range

---
+++ PrimeNumFinder/Brain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumFinder
+{
+    internal class Brain
+    {
+        public static void PrimeFinder()
+        {
+            Console.WriteLine("Enter the number :");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                if(userInput % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2)
+            {
+                Console.WriteLine(userInput + " is a prime number.");
+            }
+
+            else
+            {
+                Console.WriteLine(userInput + " is not a prime number.");
+            }
+
+        }
+
+        public static void PrimeFinderRanged() 
+        {
+            Console.WriteLine("Enter thr range :");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The prime numbers in the range of {0} are : ", userInput);
+            int tracker = 0;
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                int count = 0;
+
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 2)
+                {
+                    Console.Write(i + " ");
+                    tracker++;
+                }
+
+            }
+
+            Console.WriteLine("\n{0} prime value(s) in the range of {1}!", tracker , userInput);
+        }
+    }
+}
---
+++ PrimeNumFinder/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PrimeNumFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PRIME NUMBER FINDER");
+            Console.WriteLine("Enter 1 to find a given number is a prime or not.");
+            Console.WriteLine("Enter 2 to find all the prime numbers in the range of a given number");
+
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+            if (userChoice == 1) 
+            {
+                Brain.PrimeFinder();
+            }
+
+            else if (userChoice== 2) 
+            {
+                Brain.PrimeFinderRanged();
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid choice run again.");
+            }
+            
+        }
+    }
+}
