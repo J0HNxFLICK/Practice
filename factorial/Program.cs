20b5d1e99cd63680d33669155b114870495ad51f
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-20 11:28:37 +0530

    [Arun] Added factorial program

---
+++ factorial/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("F A C T O R I A L");
+            Test.factorialTest();
+        }
+    }
+}
---
+++ factorial/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    internal class Test
+    {
+        public static void factorialTest()
+        {
+            Console.WriteLine("Enter the number you want factorial for : ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            for (int i = userInput; i > 0; i--)
+            {
+                factorial *= i ;
+            }
+            Console.WriteLine("The factorial of {0} is : {1}",userInput, factorial);
+        }
+    }
+}
